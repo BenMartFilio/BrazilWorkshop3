4df78869729acd2b9cba5ef2fa6e2179deea9ba1
BenMartFilio <ecobmartinezfilio@gmail.com>
2026-03-18 09:08:00 +0100

    [BMF] Sacré beau menu

--- Assets/Script/MainMenu/PlaySubMenu.cs
+++ Assets/Script/MainMenu/PlaySubMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlaySubMenu : MonoBehaviour
@@ -5,6 +6,7 @@ public class PlaySubMenu : MonoBehaviour
     [SerializeField] private GameObject _playMenu;
     [SerializeField] private GameObject _playBouton;
     private RectTransform _rectBouton;
+    private Vector3 _boutonPosition;
 
     [SerializeField] private GameObject _shopMenu;
     [SerializeField] private GameObject _inventoryMenu;
@@ -12,33 +14,65 @@ public class PlaySubMenu : MonoBehaviour
     private void Start()
     {
         _rectBouton = _playBouton.GetComponent<RectTransform>();
+        _boutonPosition = _rectBouton.localPosition;
         OnPlayDisplay();
     }
     public void OnPlayDisplay()
     {
+        if (_playMenu.activeInHierarchy == true)
+        {
+            return;
+        }
         _playMenu.SetActive(true);
+        _playBouton.SetActive(true);
         _shopMenu.SetActive(false);
         _inventoryMenu.SetActive(false);
+        StartButtonDisplay();
     }
 
     private void StartButtonDisplay()
     {
+        StartCoroutine(LerpPosition(0.2f));
     }
 
 
-
+    IEnumerator LerpPosition(float time)
+    {
+        float elapsed = 0;
+        Vector3 newPosition = new Vector3 (_boutonPosition.x, _boutonPosition.y-200, _boutonPosition.z);
+        
+        _rectBouton.localPosition = newPosition;
+        
+        while (elapsed < time)
+        {
+            _rectBouton.localPosition = Vector3.Lerp(newPosition, _boutonPosition, elapsed / time);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        _rectBouton.localPosition = _boutonPosition;
+    }
 
 
     public void OnShopDisplay()
     {
+        if (_shopMenu.activeInHierarchy == true)
+        {
+            return;
+        }
         _shopMenu.SetActive(true);
         _playMenu.SetActive(false);
+        _playBouton.SetActive(false);
         _inventoryMenu.SetActive(false);
     }
     public void OnInventoryDisplay()
     {
+        if ( _inventoryMenu.activeInHierarchy == true)
+        {
+            return;
+        }
         _inventoryMenu.SetActive(true);
         _shopMenu.SetActive(false);
         _playMenu.SetActive(false);
+        _playBouton.SetActive(false);
     }
 }
